f3d9b0d82ccf531d41d110040dc36d8b69465043
Mamun Abdullah <105379063+abd-mamun@users.noreply.github.com>
2024-11-22 07:58:30 -0600

    initial commit 2

---
+++ Boolean and If statement.py
@@ -0,0 +1,16 @@
+day = 21
+weight = 190.4
+month = 'October'
+
+light_is_on = False
+
+wife_is_angry = False
+
+#if light_is_on:
+    #print("The light is on!")
+#print("Hello")
+
+if wife_is_angry:
+    print("I am screwed!")
+else:
+    print("Still screwed, awaiting confirmation!")

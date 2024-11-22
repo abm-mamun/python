9933cb68b6be5ad325b9b541f471f15e638fec87
Mamun Abdullah <mamun.offcl@gmail.com>
2024-11-22 08:14:46 -0600

    here we gi!
    first commit

---
+++ Comparison and else.py
@@ -0,0 +1,19 @@
+light_is_on = False
+
+if light_is_on:
+    print("The light is on!")
+else:
+    print("We're in the dark!!")
+
+weight = 190.4
+
+if weight < 195:
+    print("You're under weight :)")
+
+
+age = 32
+
+if age >= 18:
+    print("Damn, you are an adult :(")
+else:
+    print("Wohoo, you are a child! :D ")

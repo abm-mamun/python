25dcc504a426d9167ca98b92ca9e6c7eb972beb7
Mamun Abdullah <mamun.offcl@gmail.com>
2024-11-25 14:24:29 -0600

    Dictionaries!

---
+++ Dictionary.py
@@ -0,0 +1,17 @@
+cats = {"Jane":6, "Tom":14, "Sara":8}
+
+#Adding new key to dictionary
+cats["Wilson"] = 1
+
+#Deleting key from dictionary
+del(cats["Tom"])
+
+print(len(cats))
+
+print(cats)
+
+#Create a dictionary with Ints for keys and Booleans for values
+
+facts = {1: True, 2: False, 3: False, 4: True}
+
+print(facts)

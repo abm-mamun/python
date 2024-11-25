a1af82a495b3d16d15340bd78e574aac1de2589e
Mamun Abdullah <mamun.offcl@gmail.com>
2024-11-25 14:15:38 -0600

    For loops!

---
+++ ForLoops.py
@@ -0,0 +1,12 @@
+for number in range(10):
+    print(number)
+
+fav_movies = ["Fight Club", "Bullet Train", "Dune"]
+
+for movie in fav_movies:
+    print(movie)
+
+#loop 40 times and print the number of the loop times 2 - 2,4,6,8
+
+for numbers in range(40):
+    print((numbers + 1) * 2)

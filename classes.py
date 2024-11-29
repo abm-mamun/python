60ec6e39d2a0f82bcc4e49feb1dc36545291a23e
Mamun Abdullah <mamun.offcl@gmail.com>
2024-11-29 14:06:19 -0600

    Wohoo, class!

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Dog:
+    info = "A furry little cute creature"
+
+print(Dog.info)
+
+class Phone:
+    name = "Xiaomi Redmi"
+    model = "Note 12 4G"
+    price = 25000
+
+print(f"My smartphone is {Phone.name} {Phone.model}, and I bought it for {Phone.price} BDT")

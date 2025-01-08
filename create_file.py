98d29979335652db32e487c54983e18cd5c48a44
Mamun Abdullah <mamun.offcl@gmail.com>
2025-01-08 13:11:18 -0600

    New

---
+++ create_file.py
@@ -0,0 +1,20 @@
+#Creates only if the file does not exist
+#file = open("cheese.txt", "x")
+
+#file.write("X marks the spot")
+
+#file.close()
+
+#override
+#file = open("cheese.txt", "w")
+
+#file.write("For the W!")
+
+#file.close()
+
+#Appends
+file = open("cheese.txt", "a")
+
+file.write("A+ work!")
+
+file.close()
